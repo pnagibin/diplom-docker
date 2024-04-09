2bb7aa5adbbe8d89f2af7593f28672557df85a2b
Pavel Nagibin <pnagibin@gmail.com>
2024-04-09 22:03:01 +0300

    1

--- publish-image.sh
+++
@@ -1,4 +0,0 @@
-export CR_PAT=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
-echo $CR_PAT | docker login ghcr.io -u USERNAME --password-stdin
-docker tag 2a9521625498 ghcr.io/pnagibin/webserver:latest
-docker push ghcr.io/pnagibin/webserver:latest
